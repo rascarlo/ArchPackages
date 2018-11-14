88b8e71877f7e5df47205a87902b869bdfb3d3b8
rascarlo <rascarlo@tutanota.com>
2018-11-14 12:38:59 +0100

    rename string list adapter to dependency list adapter

--- app/src/main/java/com/rascarlo/arch/packages/adapters/DependencyListAdapter.java
+++ app/src/main/java/com/rascarlo/arch/packages/adapters/DependencyListAdapter.java
@@ -9,12 +9,12 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import com.rascarlo.arch.packages.R;
-import com.rascarlo.arch.packages.databinding.StringItemBinding;
-import com.rascarlo.arch.packages.viewholders.StringListAdapterViewHolder;
+import com.rascarlo.arch.packages.databinding.DependencyItemBinding;
+import com.rascarlo.arch.packages.viewholders.DependencyListAdapterViewHolder;
 
-public class StringListAdapter extends ListAdapter<String, StringListAdapterViewHolder> {
+public class DependencyListAdapter extends ListAdapter<String, DependencyListAdapterViewHolder> {
 
-    public StringListAdapter() {
+    public DependencyListAdapter() {
         super(DIFF_CALLBACK);
     }
 
@@ -32,16 +32,16 @@ public class StringListAdapter extends ListAdapter<String, StringListAdapterView
 
     @NonNull
     @Override
-    public StringListAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        StringItemBinding binding = DataBindingUtil.inflate(LayoutInflater.from(viewGroup.getContext()), R.layout.string_item, viewGroup, false);
-        return new StringListAdapterViewHolder(binding);
+    public DependencyListAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        DependencyItemBinding binding = DataBindingUtil.inflate(LayoutInflater.from(viewGroup.getContext()), R.layout.dependency_item, viewGroup, false);
+        return new DependencyListAdapterViewHolder(binding);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull StringListAdapterViewHolder stringListAdapterViewHolder, int i) {
+    public void onBindViewHolder(@NonNull DependencyListAdapterViewHolder dependencyListAdapterViewHolder, int i) {
         if (getItem(i) != null) {
             String s = getItem(i);
-            stringListAdapterViewHolder.bindString(s);
+            dependencyListAdapterViewHolder.bindString(s);
         }
     }
 }
--- app/src/main/java/com/rascarlo/arch/packages/viewholders/DependencyListAdapterViewHolder.java
+++ app/src/main/java/com/rascarlo/arch/packages/viewholders/DependencyListAdapterViewHolder.java
@@ -3,13 +3,13 @@ package com.rascarlo.arch.packages.viewholders;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 
-import com.rascarlo.arch.packages.databinding.StringItemBinding;
+import com.rascarlo.arch.packages.databinding.DependencyItemBinding;
 
-public class StringListAdapterViewHolder extends RecyclerView.ViewHolder {
+public class DependencyListAdapterViewHolder extends RecyclerView.ViewHolder {
 
-    private final StringItemBinding binding;
+    private final DependencyItemBinding binding;
 
-    public StringListAdapterViewHolder(@NonNull StringItemBinding binding) {
+    public DependencyListAdapterViewHolder(@NonNull DependencyItemBinding binding) {
         super(binding.getRoot());
         this.binding = binding;
     }
