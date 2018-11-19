a8cffe2337c44cc07f3e3f7211e7827b1c5bd90a
rascarlo <rascarlo@tutanota.com>
2018-11-19 11:05:10 +0100

    update copyright

--- app/src/main/java/com/rascarlo/arch/packages/viewholders/FileAdapterViewHolder.java
+++ app/src/main/java/com/rascarlo/arch/packages/viewholders/FileAdapterViewHolder.java
@@ -1,5 +1,5 @@
 /*
- *     Copyright (C) 2018 rascarlo
+ *     Copyright (C) 2018 rascarlo <rascarlo@gmail.com>
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
